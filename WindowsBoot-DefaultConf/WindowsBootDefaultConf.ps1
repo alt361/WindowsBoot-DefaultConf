753b017f4cbdc0821b2cb42eb84e5ad97f137033
root <root@DESKTOPM5BG10H.localdomain>
2026-09-07 17:07:56 +0200

    add: FHS & Script

---
+++ WindowsBoot-DefaultConf/WindowsBootDefaultConf.ps1
@@ -0,0 +1,12 @@
+﻿if( Test-Path -Path "C:\DATA")
+{
+    bcdedit /createstore C:\DATA\boot
+}
+else
+{
+    New-Item -Path C:\ -ItemType "directory" -Name DATA
+    bcdedit /createstore C:\DATA\boot 
+}
+
+bcdedit /export C:\DATA\boot
+bcdedit /store C:\DATA\boot /enum all
